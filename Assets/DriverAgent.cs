e614ae61c4d8d1505845cc5ab72b24a27a2f5619
Dominic Griffith <dgriffith6227@sdsu.edu>
2024-11-12 21:04:05 -0800

    Updated DriverAgent Class to use continuous actions instead of discrete

--- Assets/DriverAgent.cs
+++ Assets/DriverAgent.cs
@@ -29,32 +29,19 @@ public class DriverAgent : Agent
     // REQUIRES: Behavior Parameters -> Vector Observation -> Space Size = # of observations fed to agent
     public override void CollectObservations(VectorSensor sensor)
     {
-        sensor.AddObservation(transform.position);
+        sensor.AddObservation(transform.position); //counts as 3 observations (x,y,z)
     }
     
     
-    
     // Actions agent can do
-    // REQUIRES: Behavior Parameters -> Actions -> Discrete Branch & Branches = # of actions
+    // REQUIRES: Behavior Parameters -> Actions -> Discrete/Continuous Branch = # Discrete/Continuous actions
     public override void OnActionReceived(ActionBuffers actionBuffers)
     {
-        float forwardAmount = 0f;
-        float turnAmount = 0f;
+        float forwardAmount = actionBuffers.ContinuousActions[0];
+        float turnAmount = actionBuffers.ContinuousActions[1];
         bool breakAmount = false;
-
+        
         switch (actionBuffers.DiscreteActions[0])
-        {
-            case 0: forwardAmount = 0f; break; //nothing
-            case 1: forwardAmount = 1f; break; //forward
-            case 2: forwardAmount = -1f; break; //backward
-        }
-        switch (actionBuffers.DiscreteActions[1])
-        {
-            case 0: turnAmount = 0f; break; //nothing
-            case 1: turnAmount = 1f; break; //right
-            case 2: turnAmount = -1f; break; //left
-        }
-        switch (actionBuffers.DiscreteActions[2])
         {
             case 0: breakAmount = false; break; //nothing
             case 1: breakAmount = true; break; //break
@@ -66,30 +53,16 @@ public class DriverAgent : Agent
     //Used for testing
     public override void Heuristic(in ActionBuffers actionsOut)
     {
-        float horizontalInput = Input.GetAxis(HORIZONTAL);
-        float verticalInput = Input.GetAxis(VERTICAL);
         bool isBreaking = Input.GetKey(KeyCode.Space);
-
-        int forwardAction = 0;
-        int turnAction = 0;
         int boolAction = 0;
-        
-        if(horizontalInput > 0.01f) forwardAction = 1;
-        if(horizontalInput < -0.01f) forwardAction = 2;
-        if(verticalInput > 0.01f) turnAction = 1;
-        if(verticalInput < -0.01f) turnAction = 2;
         if(isBreaking) boolAction = 1;
         
         ActionSegment<int> discreteActions = actionsOut.DiscreteActions;
-        discreteActions[0] = forwardAction;
-        discreteActions[1] = turnAction;
-        discreteActions[2] = boolAction;
+        discreteActions[0] = boolAction;
         
-        // ActionSegment<int> discreteActions = actionsOut.DiscreteActions;
-        // discreteActions[0] = boolAction;
-        // ActionSegment<float> continuousActions = actionsOut.ContinuousActions;
-        // continuousActions[0] = Input.GetAxis(HORIZONTAL);;
-        // continuousActions[1] = Input.GetAxis(VERTICAL);
+        ActionSegment<float> continuousActions = actionsOut.ContinuousActions;
+        continuousActions[0] = Input.GetAxis(HORIZONTAL);;
+        continuousActions[1] = Input.GetAxis(VERTICAL);
     }
 
     // Used to determine the reward for the Agent
