557981f001b9723756a052a85ad6e855b793d50d
Kyle Krick <krkrick@gmail.com>
2025-12-02 04:19:34 +0000

    Normalized rewards, max actions 25k, added models

--- Assets/__/_Scripts/DriverAgent.cs
+++ Assets/__/_Scripts/DriverAgent.cs
@@ -51,7 +51,7 @@ public class DriverAgent : Agent
     public override void OnActionReceived(ActionBuffers actionBuffers)
     {
         // Persistent negative reward over time
-        float reward = -0.001f;
+        float reward = -0.1f;
         AddReward(reward);
 
         float forwardAmount = Mathf.Clamp(actionBuffers.ContinuousActions[0], -1f, 1f);
